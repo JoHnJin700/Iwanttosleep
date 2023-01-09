d9aa58dfd72dd71d56c58c812e606aed739b6d19
JoHnJin700 <118002660+JoHnJin700@users.noreply.github.com>
2023-01-10 02:26:33 +0900

    Add files via upload

---
+++ list/lll/src/main/java/asdf.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author a4424
+ */
+public class asdf {
+    public static void main(String[] args) {
+        System.out.println("뇽안뇽안");
+        System.out.println("안뇽안뇽");
+        System.out.println("안녕하지 못합니다");
+    }
+}
